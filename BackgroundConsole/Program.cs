6749c605d3e2c6199b2e32e5607b6f6a0c5493d7
kamilxkruk <39237618+kamilxkruk@users.noreply.github.com>
2020-02-20 16:22:24 +0100

    Add files via upload

---
+++ BackgroundConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleLib;
+
+namespace BackgroundConsole
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var funcs = new WindowsFunctions();
+            funcs.SetWallpaper("cage.jpg");
+        }
+    }
+}
---
+++ ExampleLib/WindowsFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleLib
+{
+    public class WindowsFunctions
+    {
+        [DllImport("User32", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uiAction, int uiParam, string pvParam, uint fWinIni);
+
+        public void SetWallpaper(string fileName)
+        {
+            var wallpaperName = fileName;
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var wallpaperFullPath = Path.Combine(desktopPath, wallpaperName);
+
+            SystemParametersInfo(0x0014, 0, wallpaperFullPath, 0x0001);
+        }
+
+    }
+}
